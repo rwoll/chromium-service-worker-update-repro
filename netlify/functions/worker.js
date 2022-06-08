907f59ff6e9e481040638542e134488b9df279c7
Ross A. Wollman <ross.wollman@gmail.com>
2022-06-08 18:56:49 -0400

    Init commit

---
+++ netlify/functions/worker.js
@@ -0,0 +1,13 @@
+const { randomBytes } = require('crypto');
+exports.handler = async function(event, context) {
+  const version = randomBytes(10).toString('hex');
+  return {
+    isBase64Encoded: false,
+    statusCode: 200,
+    headers: { "Content-Type": "text/javascript", "X-Version": version },
+    body: `
+      const VERSION = JSON.parse('${JSON.stringify(version)}');
+      console.log('hi from **NEW** service worker code. VERSION:', VERSION);
+    `,
+  }
+}
